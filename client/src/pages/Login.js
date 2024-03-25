5d69719f4f2820b9ef11422bf4da91c65b079464
prasathnarayanan6 <prasathnarayanan6@gmail.com>
2024-03-25 17:38:40 +0530

    Login Page V-1.0

---
+++ client/src/pages/Login.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import "@fontsource/open-sans";
+import '@fontsource/josefin-sans';
+import image from '../assets/images/nirmaan-iitm.14fdf833.svg';
+function Login() {
+    return(
+            <div className="flex w-full h-screen">
+                <div className="w-full flex items-center justify-center lg:w-1/2">
+                    <div className="bg-white px-10 py-20 rounded-xl border-2 border-blue-200">
+                    <h1 className="text-3xl font-semibold text-gray-600">LOG IN</h1>
+                            <div className="mt-7">
+                                <div>
+                                    <label className="text-lg font-medium text-blue-600">Email<span className="text-red-500">*</span></label>
+                                    <input 
+                                        className="w-full border-2 border-gray-100 rounded-l p-4 mt-1 bg-transparent"
+                                        placeholder="username@example.com"
+                                        type="email"
+                                    />
+                                </div>
+                                <div>
+                                    <label className="text-lg font-medium text-blue-600">Password<span className="text-red-500">*</span></label>
+                                    <input 
+                                        className="w-full border-2 border-gray-100 rounded-l p-4 mt-1 bg-transparent"
+                                        placeholder="Password"
+                                        type="password"
+                                    />
+                                </div>
+                                <div className="mt-8 flex justify-between items-center">
+                                    <div>
+                                        <input 
+                                            type="checkbox"
+                                            id='remember'
+                                        />
+                                        <label className="ml-2 font-medium text-base text-violet" for="remember">Remember?</label>
+                                    </div>
+                                    <button className="text-blue-500">Forgot Password</button>
+                                </div>
+                                <div className='mt-8 flex flex-col gap-y-4'>
+                                    <button className="active:scale-[.98] active:duration-75 hover:scale-[1.02] ease-in-out transition-all py-3 rounded-l bg-blue-500 text-white text-lg font-bold">Log in</button>
+                                    <button>Sign in with Google</button>
+                                </div>
+                            </div>
+
+                    </div>
+                </div>
+                <div className="hidden relative lg:flex flex-col items-center w-1/2 justify-center h-full bg-blue-300">
+                        <img src={image} alt={image} className="mb-4" width="30%"/>
+                        <div className="text-3xl font-bold mb-4">STARTUP MANAGEMENT PORTAL</div>
+                        <div className="w-full flex justify-center font-semibold">Developed by IT Team, OIE - IIT Madras</div>
+                </div>
+            </div>
+    );
+            
+}
+
+export default Login;
