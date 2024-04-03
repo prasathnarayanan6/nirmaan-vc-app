0c2c016b6597ecb50b8271563411484a34f39094
prasathnarayanan6 <prasathnarayanan6@gmail.com>
2024-04-03 12:44:53 +0530

    changes with navicons :)

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import img from '../assets/images/nirmaan-iitm.14fdf833.svg'
-import {FaPaperPlane, FaQrcode, FaRegBell, FaUserCircle } from 'react-icons/fa';
+import {FaCircleNotch, FaPaperPlane, FaQrcode, FaRegBell, FaSignOutAlt, FaUser, FaUserCircle } from 'react-icons/fa';
+import {FaGear} from 'react-icons/fa6';
 function NavBar() {
   const [isBellHovered, setIsBellHovered] = useState(false);
   const [profileHover, setProfileHover] = useState(false);
@@ -45,9 +46,8 @@ function NavBar() {
                       <a href="/" className="block py-2 px-2 text-white bg-blue-700 rounded md:bg-transparent md:text-green-500 md:p-1 dark:text-white md:dark:text-blue-500 text-xl hover:text-white hover:bg-green-400 rounded-3xl hover:rounded-2xl transition-all duration-300 ease-linear cursor-pointer group;" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} ><FaUserCircle />
                       {profileHover && (
                           <div className="absolute top-[33px;] right-0 mt-2 w-30 bg-slate-100 border border-gray-200 rounded-lg shadow-md items-center">
-                              <a href="/" className="block py-2 px-4 text-gray-700 hover:bg-gray-200 text-sm">Profile</a>
-                              <a href="/" className="block py-2 px-4 text-gray-700 hover:bg-gray-200 text-sm">Settings</a>
-                              <a href="/" className="block py-2 px-4 text-gray-700 hover:bg-gray-200 text-sm">Sign out</a>
+                              <a href="/" className="block py-2 px-4 text-gray-700 hover:bg-gray-200 text-sm"><FaGear /></a>
+                              <a href="/" className="block py-2 px-4 text-gray-700 hover:bg-gray-200 text-sm"><FaSignOutAlt /></a>
                           </div>
                       )}</a>
                   </li>
