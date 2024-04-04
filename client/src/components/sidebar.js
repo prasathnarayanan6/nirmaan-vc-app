2b8dcc6ec18b12de54cae8cd0a2a1c4395a49e74
prasathnarayanan6 <prasathnarayanan6@gmail.com>
2024-04-04 15:13:07 +0530

    changes

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -4,18 +4,31 @@ import { FaBars, FaBook, FaCalendar, FaChalkboardTeacher, FaFilePdf, FaGraduatio
 function SideBar() {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-800 text-dark shadow-lg">
-        <a href="/"><SideBarIcon icon={<FaBars size="16" className="active:scale-[.100] active:duration-75 hover:scale-[1.02] ease-in-out transition-all" text="Phone"/>} /></a>
-        <SideBarIcon icon={<FaHome size="16" className="active:scale-[.100] active:duration-75 hover:scale-[1.02] ease-in-out transition-all" text="Phone"/>} />
-        <SideBarIcon icon={<FaGraduationCap className="active:scale-[.100] active:duration-75 hover:scale-[1.02] ease-in-out transition-all" size="16" />} />
-        <SideBarIcon icon={<FaChalkboardTeacher className="active:scale-[.100] active:duration-75 hover:scale-[1.02] ease-in-out transition-all" size="16" />} />
-        <SideBarIcon icon={<FaBook className="active:scale-[.100] active:duration-75 hover:scale-[1.02] ease-in-out transition-all" size="16" />} />
-        <SideBarIcon icon={<FaCalendar className="active:scale-[.100] active:duration-75 hover:scale-[1.02] ease-in-out transition-all" size="16" />} />
-        <a href="/connections"><SideBarIcon icon={<FaPeopleArrows className="active:scale-[.100] active:duration-75 hover:scale-[1.02] ease-in-out transition-all" size="16" />} /></a>
-        <SideBarIcon icon={<FaFilePdf className="active:scale-[.100] active:duration-75 hover:scale-[1.02] ease-in-out transition-all" size="16" />} />
+      <SideBarLink href="/">
+        <SideBarIcon icon={<FaBars size="16" />} tooltipText="Bars" />
+      </SideBarLink>
+      <SideBarIcon icon={<FaHome size="16" />} tooltipText="Home" />
+      <SideBarIcon icon={<FaGraduationCap size="16" />} tooltipText="Graduation" />
+      <SideBarIcon icon={<FaChalkboardTeacher size="16" />} tooltipText="Teacher" />
+      <SideBarIcon icon={<FaBook size="16" />} tooltipText="Book" />
+      <SideBarIcon icon={<FaCalendar size="16" />} tooltipText="Calendar" />
+      <SideBarLink href="/connections">
+        <SideBarIcon icon={<FaPeopleArrows size="16" />} tooltipText="Connections" />
+      </SideBarLink>
+      <SideBarIcon icon={<FaFilePdf size="16" />} tooltipText="PDF" />
     </div>
   );
+}
+
+const SideBarLink = ({ href, children }) => {
+  return (
+    <a href={href} className="block">
+      {children}
+    </a>
+  );
 };
-const SideBarIcon = ({ icon, text = "Home" }) => {
+
+const SideBarIcon = ({ icon, tooltipText }) => {
   const [hoverTooltip, setHoverTooltip] = useState(false);
 
   const handleMouseEnter = () => {
@@ -35,7 +48,7 @@ const SideBarIcon = ({ icon, text = "Home" }) => {
       {icon}
       {hoverTooltip && (
         <span className="absolute w-auto p-2 m-2 min-w-max left-14 rounded-md shadow-md text-white bg-gray-900 text-xs font-bold transition-all duration-100 scale-100">
-            {text}
+            {tooltipText}
         </span>
       )}
     </div>
