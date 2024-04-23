cf648a33bd97f52f084a69a080b8379b487cb6cb
prasathnarayanan6 <prasathnarayanan6@gmail.com>
2024-04-23 17:00:04 +0530

    Changes in API based on admin level login

--- server/model/LoginModel.js
+++ server/model/LoginModel.js
@@ -1,7 +1,7 @@
 const client = require('../utils/conn');
 const LoginModel = (user_mail, user_password) => {
     return new Promise((resolve, reject) => {
-        client.query(`SELECT user_mail, user_password, user_department FROM user_data WHERE user_mail=$1 AND user_password=$2`,
+        client.query(`SELECT user_mail, user_role, user_password, user_department FROM user_data WHERE user_mail=$1 AND user_password=$2`,
         [user_mail, user_password],
         (error, result)=> {
             if (error) 
@@ -12,9 +12,9 @@ const LoginModel = (user_mail, user_password) => {
             {
                 if(result.rows.length > 0)
                 {
-                    if(result.rows[0].user_department === "Tech")
+                    if(result.rows[0].user_role === 2)
                     {
-                        resolve({})
+                        console.log("admin")
                     }
                 }
                 else
