e87724057beb7c1a40cbe36e45eba899bef64427
prasathnarayanan6 <prasathnarayanan6@gmail.com>
2024-04-23 16:26:33 +0530

    API for login done

--- server/controller/LoginController/LoginController.js
+++ server/controller/LoginController/LoginController.js
@@ -1,15 +1,22 @@
 const LoginModel = require('../../model/LoginModel')
 const LoginController = async (req, res) => {
-    try
+    const{user_mail, user_password} = req.body
+    if(user_mail && user_password)
     {
-        const {user_mail, user_password} = req.body
-        const result = LoginModel(user_mail, user_password);
-        res.status(200).json({status: result});
+        try
+        {
+            const result = await LoginModel(user_mail, user_password);
+            res.status(200).json({result });
+        }
+        catch (err)
+        {
+            console.log(err);
+            res.status(200).json({error: 'Internal Server Error'});
+        }
     }
-    catch (err)
+    else 
     {
-        console.log(err);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
+		res.send('Please enter Username and Password!');
+	}
 }
 module.exports = LoginController;
--- server/model/LoginModel.js
+++ server/model/LoginModel.js
@@ -1,7 +1,7 @@
 const client = require('../utils/conn');
 const LoginModel = (user_mail, user_password) => {
     return new Promise((resolve, reject) => {
-        client.query(`SELECT user_mail, user_password, user_department FROM user_data WHERE user_mail=? && user_password=?`,
+        client.query(`SELECT user_mail, user_password, user_department FROM user_data WHERE user_mail=$1 AND user_password=$2`,
         [user_mail, user_password],
         (error, result)=> {
             if (error) 
@@ -10,7 +10,15 @@ const LoginModel = (user_mail, user_password) => {
             }
             else
             {
-                resolve(result);
+                if(result.rows.length > 0)
+                {
+                    resolve({ authenticated: true, user: result.rows[0]});
+                }
+                else
+                {
+                    resolve({authenticated: false});
+                }
+                resolve({result});
             }
         })
     })
